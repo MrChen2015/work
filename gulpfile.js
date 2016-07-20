2f0d22d608a1dcd0f64f9c4156495a3709a911a0
MrChen2015 <562638230@qq.com>
2016-07-20 18:25:22 +0800

    cc

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,91 @@
-// 引入 gulp
-var gulp = require('gulp'); 
+'use strict';
+// 载入外挂
 
-// 引入组件
-var jshint = require('gulp-jshint');
-// var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+var gulp = require('gulp'),
+ sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),//添加浏览器标识的插件
+    minifycss = require('gulp-clean-css'),
+    // jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    // imagemin = require('gulp-imagemin'),
+    // rename = require('gulp-rename'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify');
+    // cache = require('gulp-cache'),
+    // livereload = require('gulp-livereload');
 
-// 检查脚本
-// gulp.task('lint', function() {
-//     return gulp.src('./src/js/*.js')
-//         .pipe(jshint())
-//         .pipe(jshint.reporter('default'));
-// });
-
-// 编译Sass
-// gulp.task('sass', function() {
-//     return gulp.src('./src/sass/*.scss')
-//         .pipe(sass())
-//         .pipe(gulp.dest('./src/css'));
-// });
-
-// 合并，压缩文件
-// gulp.task('scripts', function() {
-//     gulp.src('./src/js/*.js')
-//         .pipe(concat('all.js'))
-//         .pipe(gulp.dest('./dist/js'))
-//         .pipe(rename('all.min.js'))
-//         .pipe(uglify())
-//         .pipe(gulp.dest('./dist/js'));
-// });
-
-gulp.task("compress",function(){
-    return gulp.src("src/js/*.js")
-    .pipe(uglify())
-    .pipe(gulp.dest("dist/js/main.js"));
+gulp.task('sass', function () {
+    return sass('src/sass/*.scss')
+        .on('error', sass.logError)
+        .pipe(gulp.dest('dist/css'));
 });
-// 默认任务
-// gulp.task('default', function(){
-//     gulp.run('lint', 'sass', 'scripts');
+// 样式
+gulp.task('styles', function() {
+    return gulp.src('src/css/*.css')
+        // .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+        }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(concat('hello.css'))
+        .pipe(gulp.dest('dist/css'))
+        // .pipe(rename({ suffix: '.min' }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(notify({ message: 'Styles task complete' }));
+});
 
-    // 监听文件变化
-//     gulp.watch('./src/js/*.js', function(){
-//         gulp.watch('lint', 'sass', 'scripts');
+// 脚本
+gulp.task('scripts', function() {
+    return gulp.src('src/scripts/**/*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/scripts'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/scripts'))
+        .pipe(notify({ message: 'Scripts task complete' }));
+});
+//
+// // 图片
+// gulp.task('images', function() {
+//     return gulp.src('src/images/**/*')
+//         .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+//         .pipe(gulp.dest('dist/images'))
+//         .pipe(notify({ message: 'Images task complete' }));
+// });
+//
+// // 清理
+// gulp.task('clean', function() {
+//     return gulp.src(['dist/styles', 'dist/scripts', 'dist/images'], {read: false})
+//         .pipe(clean());
+// });
+//
+// // 预设任务
+// gulp.task('default', ['clean'], function() {
+//     gulp.start('styles', 'scripts', 'images');
+// });
+//
+// // 看守
+// gulp.task('watch', function() {
+//
+//     // 看守所有.scss档
+//     gulp.watch('src/styles/**/*.scss', ['styles']);
+//
+//     // 看守所有.js档
+//     gulp.watch('src/scripts/**/*.js', ['scripts']);
+//
+//     // 看守所有图片档
+//     gulp.watch('src/images/**/*', ['images']);
+//
+//     // 建立即时重整伺服器var server = livereload();
+//
+//     // 看守所有位在 dist/  目录下的档案，一旦有更动，便进行重整
+//     gulp.watch(['dist/**']).on('change', function(file) {
+//         server.changed(file.path);
 //     });
+//
 // });
--- src/js/base.js
+++ src/js/base.js
@@ -1,3 +1,6 @@
 /**
  * Created by Administrator on 2016/7/18.
  */
+;(function(){
+    alert("base.js");
+}())
